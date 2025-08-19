dc6b7b8ced6d955e6f7681932ff232219f62a2ec
Harichara <bryanharicharan3@yahoo.com>
2025-08-19 19:00:32 -0400

    adding folder structure

--- prop-man-api/src/app.module.ts
+++ prop-man-api/src/app.module.ts
@@ -3,10 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SupabaseService } from './supabase/supabase.service';
 import { LeasesController } from './leases/leases.controller';
+import { ConfigModule } from '@nestjs/config';
+import { LeasesService } from './leases/leases.service';
+import { LeasesModule } from './leases/leases.module';
+import { TenantsController } from './tenants/tenants.controller';
+import { TenantsService } from './tenants/tenants.service';
+import { TenantsModule } from './tenants/tenants.module';
+import { PropertiesController } from './properties/properties.controller';
+import { PropertiesService } from './properties/properties.service';
+import { PropertiesModule } from './properties/properties.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, LeasesController],
-  providers: [AppService, SupabaseService],
+  imports: [ConfigModule.forRoot(), LeasesModule, TenantsModule, PropertiesModule],
+  controllers: [AppController, LeasesController, TenantsController, PropertiesController],
+  providers: [AppService, SupabaseService, LeasesService, TenantsService, PropertiesService],
 })
 export class AppModule {}
--- prop-man-api/src/leases/leases.controller.ts
+++ prop-man-api/src/leases/leases.controller.ts
@@ -1,18 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { SupabaseService } from '../supabase/supabase.service';
+import { Lease } from '../types/lease';
 
 @Controller('leases')
 export class LeasesController {
   constructor(private readonly supabase: SupabaseService) {}
 
   @Get()
-  async findAll() {
+  async findAll(): Promise<Lease[]> {
     const { data, error } = await this.supabase
       .getClient()
-      .from('leases')
+      .from<Lease>('leases')
       .select('*');
 
     if (error) throw error;
-    return data;
+    return data ?? [];
   }
 }
